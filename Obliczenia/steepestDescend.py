299b0f99c1a068a470f6bde2820ea0aca2fbb3d9
JanMaciuk <38810823+JanMaciuk@users.noreply.github.com>
2024-04-16 12:12:21 +0200

    Change stop condition to accuracy value

--- Obliczenia/steepestDescend.py
+++ Obliczenia/steepestDescend.py
@@ -3,18 +3,19 @@ import numpy as np
 def gradient_f(a, b):
     return np.array([2*a, 2*b]) # for f(x, y) = x^2 + y^2
 
-def steepest_descent_method(start_point, step_length, min_gradient):
+def steepest_descent_method(start_point, step_length, min_accuracy):
     current_point = np.array(start_point)
     iteration = 0
     
     while True:
         gradient = gradient_f(*current_point)
-        print("Iteracja", iteration, ": x =", current_point, "Gradient:", gradient, "Długość gradientu:", np.linalg.norm(gradient))
+        next_point = current_point - step_length * gradient
+        accuracy = round(np.linalg.norm(next_point - current_point),4)
+        print("Iteracja", iteration, ": x =", current_point, "Gradient:", gradient, "Dokładność:", accuracy)
         
-        if np.linalg.norm(gradient) < min_gradient:
+        if accuracy < min_accuracy:
             break
         
-        next_point = current_point - step_length * gradient
         current_point = next_point
         iteration += 1
     
@@ -23,6 +24,6 @@ def steepest_descent_method(start_point, step_length, min_gradient):
 # Test
 start_point = [4, 4]
 step_length = 0.3
-min_gradient = 1e-2
+min_accuracy = 1e-2
 
-steepest_descent_method(start_point, step_length, min_gradient)
+steepest_descent_method(start_point, step_length, min_accuracy)
