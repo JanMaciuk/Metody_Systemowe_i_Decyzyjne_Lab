2902bb7d80893300386f663acfc525d81c8db62b
JanMaciuk <38810823+JanMaciuk@users.noreply.github.com>
2024-06-05 18:13:11 +0200

    Save graph to file

--- Projekt/Graphing.py
+++ Projekt/Graphing.py
@@ -1,4 +1,4 @@
-import pandas, matplotlib.pyplot
+import pandas, matplotlib.pyplot, os
 
 LABEL_MAGNETIC_FIELD:str = "Magnetic field strength B (µT)"
 LABEL_CURRENT:str = "Current I (mA)"
@@ -6,7 +6,7 @@ LABEL_TIME:str = "Time t (s)"
 LABEL_BACKGROUND_TEMP:str = "Background Temperature T (°C)"
 
 
-def graph_data_against_time(dataFrame: pandas.DataFrame, current = True, magneticField = True) -> None:
+def graph_data_against_time(dataFrame: pandas.DataFrame, current = True, magneticField = True, show=False) -> None:
     """
     Plot the experiment data from a DataFrame,
     Time is the x-axis, current and magnetic field on y-axis.
@@ -21,4 +21,17 @@ def graph_data_against_time(dataFrame: pandas.DataFrame, current = True, magneti
     matplotlib.pyplot.xlabel(LABEL_TIME)
     matplotlib.pyplot.ylabel("Current / Magnetic Field")
     matplotlib.pyplot.legend()
-    matplotlib.pyplot.show()
+    if show:
+        matplotlib.pyplot.show()
+    else:
+        #Save as Plot1, if it already exists save as Plot2, etc.
+        filePath:str = os.path.dirname(os.path.realpath(__file__))  # Get the path of the current file
+        plotNumber = 1
+        filePath = os.path.join(filePath, "Plots")
+        while os.path.isfile(os.path.join(filePath, "Plot"+str(plotNumber)+".png")):
+            plotNumber += 1
+        filePath = os.path.join(filePath, "Plot"+str(plotNumber)+".png")
+        matplotlib.pyplot.savefig(filePath)
+        matplotlib.pyplot.close()
+
+
